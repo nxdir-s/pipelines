5ad2e175a5678d894a76679ae370a40704118551
nadir <snadir177@gmail.com>
2024-08-16 23:07:58 -0400

    pass func to go routine in fanout

--- pipelines.go
+++ pipelines.go
@@ -67,7 +67,7 @@ func FanOut[T any, H any](ctx context.Context, inputStream <-chan T, fn func(con
 	process := func() <-chan H {
 		stream := make(chan H)
 
-		go func() {
+		go func(f func(context.Context, T) H) {
 			defer close(stream)
 
 			for value := range inputStream {
@@ -76,10 +76,10 @@ func FanOut[T any, H any](ctx context.Context, inputStream <-chan T, fn func(con
 					return
 				default:
 					// process data with supplied function
-					stream <- fn(ctx, value)
+					stream <- f(ctx, value)
 				}
 			}
-		}()
+		}(fn)
 
 		return stream
 	}
